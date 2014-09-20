3bd334ca6c4a9e7ee3428521c765a495cce50fab
lijian17 <374452668@qq.com>
2014-09-20 14:42:56 +0800

    菜单界面完成

--- plantsVSzombies/src/net/dxs/plantsvszombies/MainActivity.java
+++ plantsVSzombies/src/net/dxs/plantsvszombies/MainActivity.java
@@ -1,15 +1,41 @@
 package net.dxs.plantsvszombies;
 
-import android.os.Bundle;
+import net.dxs.plantsvszombies.layer.WelcomeLayer;
+
+import org.cocos2d.layers.CCScene;
+import org.cocos2d.nodes.CCDirector;
+import org.cocos2d.opengl.CCGLSurfaceView;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		//你好
+		//获得一个surfaceView
+		CCGLSurfaceView surfaceView = new CCGLSurfaceView(this);
+		setContentView(surfaceView);
+
+		//获得一个导演
+		CCDirector director = CCDirector.sharedDirector();
+		director.attachInView(surfaceView);//开启绘制线程
+
+		director.setDeviceOrientation(CCDirector.kCCDeviceOrientationLandscapeLeft);
+		director.setScreenSize(480, 320);
+
+		director.setDisplayFPS(true);//显示帧率
+
+		//获得一个场景
+		CCScene scene = CCScene.node();
+
+		WelcomeLayer layer = new WelcomeLayer();
+
+		//添加图层
+		scene.addChild(layer);
+
+		//运行场景
+		director.runWithScene(scene);
 	}
 }
